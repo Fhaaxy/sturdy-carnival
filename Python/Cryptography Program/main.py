cc7a284d5934d6246ac13fa302a9fae9c61e7ac6
Fhaaxy <albinicalogero29@gmail.com>
2026-02-27 14:32:08 +0100

    Added functions

--- Python/Cryptography Program/main.py
+++ Python/Cryptography Program/main.py
@@ -1,5 +1,6 @@
 import codecs
 import base64
+import morse_talk
 import hashlib
 
 
@@ -50,6 +51,31 @@ def base64_decrypt(text):
 
 
 
+#Morse Code
+def morse_encrypt(text):
+    return morse_talk.encode(text)
+
+def morse_decrypt(text):
+    return morse_talk.decode(text)
+
+
+
+#Atbash
+def atbash(text):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            if char.isupper():
+                base = 65
+            else:
+                base = 97
+            result += chr(base + (25 - (ord(char) - base)))
+        else:
+            result += char
+    return result
+
+
+
 #Hashing (SHA256)
 def hash_sha256(text):
     return hashlib.sha256(text.encode()).hexdigest()
@@ -65,31 +91,45 @@ while True:
 
     if choice1 == "D":
         print("What is the method of encryption? ")
-        print("A) Ceasar Cipher")
-        print("B) ROT13")
-        print("C) Base64")
+        print("1) Ceasar Cipher")
+        print("2) ROT13")
+        print("3) Base64")
+        print("4) Morse Code")
+        print("5) Atbash")
         print("0) Back")
 
         decryption = input().upper()
 
         while True:
-            if decryption == "A":
+            if decryption == "1":
                 print("Ceasar Cipher:")
-                text = input("Insert Text:")
+                text = input("Insert Text: ")
                 shift = int(input("Select Shift: "))
                 print(ceasar_decrypt(text, shift))
                 break
 
-            elif decryption == "B":
+            elif decryption == "2":
                 print("ROT13")
-                text = input("Insert Text:")
+                text = input("Insert Text: ")
                 print(rot13_decrypt(text))
                 break
 
-            elif decryption == "C":
+            elif decryption == "3":
                 print("Base64")
-                text = input("Insert Text:")
+                text = input("Insert Text: ")
                 print(base64_decrypt(text))
+                break
+
+            elif decryption == "4":
+                print("Morse Code")
+                text = input("Insert Text: ")
+                print(morse_decrypt(text))
+                break
+
+            elif decryption == "5":
+                print("Atbash")
+                text = input("Insert Text: ")
+                print(atbash(text))
                 break
 
             elif decryption == "0":
@@ -100,37 +140,51 @@ while True:
 
     elif choice1 == "E":
         print("Which type of Cryptography do you choose? ")
-        print("A) Ceasar Cipher")
-        print("B) ROT13")
-        print("C) Base64")
-        print("D) Hash_Sha256")
+        print("1) Ceasar Cipher")
+        print("2) ROT13")
+        print("3) Base64")
+        print("4) Morse Code")
+        print("5) Atbash")
+        print("6) Hash_Sha256")
         print("0) Back")
 
         encryption = input().upper()
 
         while True:
-            if encryption == "A":
+            if encryption == "1":
                 print("Ceasar Cipher")
-                text = input("Insert Text:")
+                text = input("Insert Text: ")
                 shift = int(input("Select Shift: "))
                 print(caesar_encrypt(text, shift))
                 break
 
-            elif encryption == "B":
+            elif encryption == "2":
                 print("ROT13")
-                text = input("Insert Text:")
+                text = input("Insert Text: ")
                 print(rot13_encrypt(text))
                 break
 
-            elif encryption == "C":
+            elif encryption == "3":
                 print("Base64")
-                text = input("Insert Text:")
+                text = input("Insert Text: ")
                 print(base64_encrypt(text))
                 break
 
-            elif encryption == "D":
+            elif encryption == "4":
+                print("Morse Code")
+                text = input("Insert Text: ")
+                print(morse_encrypt(text))
+                break
+
+            elif encryption == "5":
+                print("Atbash")
+                text = input("Insert Text: ")
+                print(atbash(text))
+                break
+
+            elif encryption == "6":
                 print("Hash_Sha256")
-                text = input("Insert Text:")
+                text = input("Insert Text: ")
                 print(hash_sha256(text))
                 break
 
